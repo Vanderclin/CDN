c525387eb3c19c13dadc97889e7572768cbba42b
Vanderclin Rocha <vanderclin@users.noreply.github.com>
2019-09-01 13:07:11 -0300

    New update

--- js/custom.js
+++
@@ -1,6 +0,0 @@
-$('.test, .nav-link, .navbar-brand, .new-button').click(function () {
-    var sectionTo = $(this).attr('href');
-    $('html, body').animate({
-        scrollTop: $(sectionTo).offset().top
-    }, 1500);
-});
