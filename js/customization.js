c0b0523d28746a1580f0fe80a9c842d0e9c34810
Vanderclin <vanderclinrocha@gmail.com>
2020-07-08 07:06:26 -0300

    New update

---
+++ js/customization.js
@@ -0,0 +1,4 @@
+document.getElementById("id_title").innerHTML = "O arquivo a ser baixado é um teste de 5 dias, está sendo atualizado frequentemente.</b>";
+document.getElementById("id_created").innerHTML = "Criado: 08/07/2020";
+document.getElementById("id_expires").innerHTML = "Expira: 13/07/2020";
+document.getElementById("id_server").innerHTML = "Servidor: Canadá";
