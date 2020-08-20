dd2f5df5c25cc161f28f1fe8e23ab17259180a5b
Vanderclin Rocha <36233501+Vanderclin@users.noreply.github.com>
2020-08-20 17:00:32 -0300

    New update

--- js/customization.js
+++ js/customization.js
@@ -1,3 +1,16 @@
 function openModalSignIn() {
     $("#modal_sign_in").modal("show");
 }
+var swiper = new Swiper('.swiper-container', {
+    spaceBetween: 30,
+    centeredSlides: true,
+    loop: true,
+    autoplay: {
+        delay: 8000,
+        disableOnInteraction: false,
+    },
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+    },
+});
