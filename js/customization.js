107c358c1301d8a3b3a5998a5b1a64238f719c66
Vanderclin <vanderclinrocha@gmail.com>
2020-08-21 11:44:19 -0300

    New update

--- js/customization.js
+++ js/customization.js
@@ -1,23 +1,33 @@
 function openModalSignIn() {
-    $("#modal_sign_in").modal("show");
+	$("#modal_sign_in").modal("show");
 }
 var swiper = new Swiper('.swiper-container', {
-    spaceBetween: 30,
-    centeredSlides: true,
-    loop: true,
-    autoplay: {
-        delay: 8000,
-        disableOnInteraction: false,
-    },
+	spaceBetween: 30,
+	centeredSlides: true,
+	loop: true,
+	autoplay: {
+		delay: 8000,
+		disableOnInteraction: false,
+	},
 });
 
-
+/*
 $(document).ready(function () {
-    console.log("document is ready");
-    $('[data-toggle="offcanvas"], #menu').on('click', function () {
-        $('.offcanvas-collapse').toggleClass('open')
-    })
+	console.log("document is ready");
+	$('[data-toggle="offcanvas"], #menu').on('click', function () {
+		$('.offcanvas-collapse').toggleClass('open');
+	});
 });
 window.onload = function () {
-    console.log("window is loaded");
+	console.log("window is loaded");
 };
+*/
+$(document).ready(function () {
+	$("#menu").click(function () {
+		if ($("input[type=checkbox]").is(":checked")) {
+			$('.offcanvas-collapse').toggleClass('open');
+		} else {
+			$('.offcanvas-collapse').toggleClass('open');
+		}
+	});
+});
