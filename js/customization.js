ab999d4c9f0bfda1453bd6a86fec017c3cf94eaa
Vanderclin Rocha <36233501+Vanderclin@users.noreply.github.com>
2020-08-23 21:35:57 -0300

    New update test

--- js/customization.js
+++ js/customization.js
@@ -1,14 +1,14 @@
 function openModalSignIn() {
-	$("#modal_sign_in").modal("show");
+    $("#modal_sign_in").modal("show");
 }
 var swiper = new Swiper('.swiper-container', {
-	spaceBetween: 30,
-	centeredSlides: true,
-	loop: true,
-	autoplay: {
-		delay: 8000,
-		disableOnInteraction: false,
-	},
+    spaceBetween: 30,
+    centeredSlides: true,
+    loop: true,
+    autoplay: {
+        delay: 8000,
+        disableOnInteraction: false,
+    },
 });
 
 /*
@@ -23,19 +23,19 @@ window.onload = function () {
 };
 */
 $(document).ready(function () {
-	$("#menu").click(function () {
-		if ($("input[type=checkbox]").is(":checked")) {
-			$('.offcanvas-collapse').toggleClass('open');
-		} else {
-			$('.offcanvas-collapse').toggleClass('open');
-		}
-	});
+    $("#menu").click(function () {
+        if ($("input[type=checkbox]").is(":checked")) {
+            $('.offcanvas-collapse').toggleClass('open');
+        } else {
+            $('.offcanvas-collapse').toggleClass('open');
+        }
+    });
 });
 
-$( "#input_user_name" ).on('input', function() {
-    if ($(this).val().length>=16) {
-		document.getElementById("input_user_name").disabled = true;
-		document.getElementById("buttonUpdate").disabled = false;
-		$('.collapse').collapse();
+$("#input_user_name").on('input', function () {
+    if ($(this).val().length >= 16) {
+        document.getElementById("input_user_name").disabled = true;
+        document.getElementById("buttonUpdate").disabled = false;
+        $('.collapse').collapse();
     }
 });
